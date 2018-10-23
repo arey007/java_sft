2b3cd1f0d2e506adaee73b1dcffa510d64641f13
arey007 <arey007@mail.ru>
2018-10-23 22:35:56 +0700

    add a few func

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        hello("Alex");
 
+        double l = 8;
+        System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+        double a = 5;
+        double b = 4;
+        System.out.println("Площадь прямоугольника со сторонами " +a + " и " +b + " равна " + area(a,b));
+    }
+
+    public static void hello(String somebody) {
+
+        System.out.println("hi, " + somebody);
+    }
+
+    public static double area (double len){
+        return len*len;
+    }
+
+    public static double area (double a, double b){
+        return  a * b;
     }
 }
